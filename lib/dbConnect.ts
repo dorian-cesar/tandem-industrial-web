8dcc83b4bdc4f51800f4452ba422bb15137f962b
diego <dwigodski@gmail.com>
2025-08-27 15:56:27 -0400

    fix: se modifica dbConnect

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,13 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
-}
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } 
+prisma = new PrismaClient();
+// else {
+//   if (!(global as any).prisma) {
+//     (global as any).prisma = new PrismaClient();
+//   }
+//   prisma = (global as any).prisma;
+// }
 
 export default prisma;
